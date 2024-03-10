704f18080340002b813202b7e58d61ea2fb7120c
vinay ch <chvinay8288@gmail.com>
2024-03-10 20:29:57 +0530

    nav fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,7 @@
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 
           <div class="flex flex-shrink-0 items-center">
-    
+
             <img class="h-12 w-auto px-10" src="../assets/logo.png" alt="Logo" />
 
           </div>
@@ -48,23 +48,27 @@
 
                     <MenuItem v-slot="{ active }">
 
-                    <a href="/#/chiefpatrons" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief Patronsn</a>
+                    <a href="/#/chiefpatrons"
+                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief
+                      Patron</a>
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
                     <a href="/#/patronsadvisors"
-                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Patrons & Advisors</a>
-                    </MenuItem>
-
-                    <MenuItem v-slot="{ active }">
-                    <a href="/#/team"
-                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief Co-ordinators</a>
+                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Patrons
+                      & Advisors</a>
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
                     <a href="/#/conveners"
                       :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Conveners</a>
                     </MenuItem>
 
-               
+                    <MenuItem v-slot="{ active }">
+                    <a href="/#/team"
+                      :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief Student
+                      Co-ordinators</a>
+                    </MenuItem>
+
+
                   </MenuItems>
                 </transition>
               </Menu>
@@ -105,7 +109,8 @@
 
                 <MenuItem v-slot="{ active }">
 
-                <a href="/#/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Profile</a>
+                <a href="/#/profile"
+                  :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Profile</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
 
@@ -120,16 +125,64 @@
     </div>
 
     <DisclosurePanel class="sm:hidden">
-      <div class="space-y-1 px-2 pb-3 pt-2">
+      <div class="px-2 pb-3 pt-2 flex justify-evenly">
         <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-          :class="['text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+          :class="['text-gray-300 hover:bg-gray-700 hover:text-white ', 'rounded-md px-3 py-2 text-sm font-medium']"
           :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+
         <template v-if="!authStore.isAuth" class="hidden md:block">
 
           <a href="/#/signup"
             :class="['text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']">Register</a>
-            
+
         </template>
+
+        <Menu as="div" class="relative">
+          <div>
+            <MenuButton
+              class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">Team</span>
+              Team
+            </MenuButton>
+          </div>
+          <transition enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95">
+            <MenuItems
+              class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-900 hover:text-gray-900 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <!-- <MenuItem v-slot="{ active }">
+                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">My
+                  Events</a>
+                </MenuItem> -->
+
+              <MenuItem v-slot="{ active }">
+
+              <a href="/#/chiefpatrons"
+                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief
+                Patron</a>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+              <a href="/#/patronsadvisors"
+                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Patrons
+                & Advisors</a>
+              </MenuItem>
+              <MenuItem v-slot="{ active }">
+              <a href="/#/conveners"
+                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Conveners</a>
+              </MenuItem>
+
+              <MenuItem v-slot="{ active }">
+              <a href="/#/team"
+                :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-50 hover:text-gray-900 ']">Chief Student
+                Co-ordinators</a>
+              </MenuItem>
+
+
+            </MenuItems>
+          </transition>
+        </Menu>
       </div>
     </DisclosurePanel>
   </Disclosure>
