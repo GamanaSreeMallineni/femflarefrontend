0db751b1e770739a2ea086edf4a483a094fead81
GAMANASREE8985 <2200030228@klunibversity.in>
2025-03-26 12:42:24 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/femflare-front1/',
+  base: '/femflarefrontend/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
