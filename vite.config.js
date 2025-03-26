99322035819a307b2690373f6ff9884081496d82
GamanaSreeMallineni <147695652+GamanaSreeMallineni@users.noreply.github.com>
2025-03-26 11:19:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'femflare-frontend',
+  base: 'femflare-front1',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
